60e1f134a0c79a5516252fecb1661e6e6bc2cee7
jcindex <1259145811@qq.com>
2016-08-31 10:11:42 +0800

    modified login_session.js

--- src/common/middleware/login_session.js
+++ src/common/middleware/login_session.js
@@ -18,14 +18,32 @@ export default class extends think.middleware.base {
     let http = this.http;
     let url = http.req.url;
     let admin = yield http.session('admin');
-    // admin = 1;
-    console.log(">>>", url.match(/^\/admin\/(?!(login|?!image))(.*)$/));
-    // if(url.match(/^\/admin\/(?!login)(.*)$/)) {
-    //   if(!admin) {
-    //     http.redirect("/admin/login");
-    //     return think.prevent();
-    //   }
-    // }
+    admin = 1;
+    if(url.lastIndexOf('/') !== url.length - 1) url += "/";
+    if(url.match(/^\/admin\/(([^/]*)?\/?)?(([^/]*)?\/?)*/)) {
+      let _2 = RegExp.$2;
+      let _4 = RegExp.$4;
+      // console.log(">>>", (_2 !== "index" && _4 !== "login"), _2, _4);
+      if((_2 === "index" && _4 === 'login')
+        || (_2 === "login")) {
+        //登录请求
+        console.log("登录请求");
+        if(admin) {
+          http.redirect("/admin/");
+          return http.prevent();
+        }
+      } else if((_2 === 'index' && _4 === 'index')
+        || (_2 === "index" && !_4)
+        || (_2 === "index" && _4 !== "login")
+        || (!_2 && !_4)) {
+        //后台非登录请求
+        console.log("后台非登录请求");
+        if(!admin) {
+          http.redirect("/admin/login");
+          return http.prevent();
+        }
+      }
+    }
     return {};
   }
 }
